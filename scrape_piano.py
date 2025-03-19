c524d894fc39f2010c36ed83ca33863fbe99b8be
Brais <braisrg5@gmail.com>
2025-03-19 23:14:18 +0100

    First tries

--- scrape_piano.py
+++ scrape_piano.py
@@ -1,29 +1,8 @@
-from __future__ import print_function
-from datetime import datetime
-import os
-import pip
+'''Program to scrape the page https://www.pianolibrary.org/difficulty/'''
+from bs4 import BeautifulSoup
+import requests
 
 
-if __name__ == "__main__":
-    packages = []
-    for package in pip.get_installed_distributions():
-        package_name_version = str(package)
-        try:
-            module_dir = next(package._get_metadata('top_level.txt'))
-            package_location = os.path.join(package.location, module_dir)
-            os.stat(package_location)
-        except (StopIteration, OSError):
-            try:
-                package_location = os.path.join(package.location, package.key)
-                os.stat(package_location)
-            except:
-                package_location = package.location
-        modification_time = os.path.getctime(package_location)
-        modification_time = datetime.fromtimestamp(modification_time)
-        packages.append([
-            modification_time,
-            package_name_version
-        ])
-    for modification_time, package_name_version in sorted(packages):
-        print("{0} - {1}".format(modification_time,
-                                 package_name_version))
+if __name__ == '__main__':
+    URL = 'https://www.pianolibrary.org/difficulty/'
+    difficulty = requests.get(URL, timeout=10)
