0cca84cb3e8e8d9fb811ef5c9369a8e14d885906
Brais <braisrg5@gmail.com>
2025-03-19 23:18:10 +0100

    Yes

--- scrape_piano.py
+++ scrape_piano.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import requests
 
 
+def main():
+    '''Main program.'''
+    url = 'https://www.pianolibrary.org/difficulty/'
+    difficulty = requests.get(url, timeout=10)
+    soup = BeautifulSoup(difficulty.text, 'html.parser')
+    print(soup)
+
+
 if __name__ == '__main__':
-    URL = 'https://www.pianolibrary.org/difficulty/'
-    difficulty = requests.get(URL, timeout=10)
+    main()
